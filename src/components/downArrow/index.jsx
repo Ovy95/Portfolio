457c2aebcac9da3200c4b164ae08de55f21694c9
Ovy95 <jackoverton21@hotmail.com>
2021-03-21 12:58:13 +0000

    Downarrow Component added

---
+++ src/components/downArrow/index.jsx
@@ -0,0 +1,39 @@
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styled from 'styled-components';
+
+const ArrowContainer = styled.div`
+width: 50px;
+height: 50px;
+border-radius : 50%;
+background-color : red;
+display: flex;
+justify-content: center;
+align-items: center;
+border:2px solid transparent;
+transition: all 250ms ease-in-out;
+cursor: pointer;
+ 
+
+&:hover {
+  border :2px solid black
+}
+`;
+
+const ArrowIcon = styled.div`
+margin-top: 3px;
+color : #fff;
+font-size: 30px;
+`
+
+
+
+export function DownArrow() {
+  return <ArrowContainer>
+    <ArrowIcon>
+      <FontAwesomeIcon icon={faAngleDown} />
+    </ArrowIcon>
+  </ArrowContainer>
+
+}
