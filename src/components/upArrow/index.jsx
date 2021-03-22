f7d33c6ecb15bf370316c27f3e87a0fcf8a246b8
Ovy95 <jackoverton21@hotmail.com>
2021-03-22 16:27:25 +0000

    Upscoller function added

---
+++ src/components/upArrow/index.jsx
@@ -0,0 +1,40 @@
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styled from 'styled-components';
+
+const ArrowContainer = styled.div`
+width: 50px;
+height: 50px;
+border-radius : 50%;
+background-color : red;
+display: flex;
+justify-content: center;
+align-items: center;
+border:2px solid transparent;
+transition: all 250ms ease-in-out;
+cursor: pointer;
+
+ 
+
+&:hover {
+  border :2px solid black
+}
+`;
+
+const ArrowIcon = styled.div`
+margin-top: 3px;
+color : #fff;
+font-size: 30px;
+`
+
+
+
+export function UpArrow() {
+  return <ArrowContainer>
+    <ArrowIcon>
+      <FontAwesomeIcon icon={faAngleUp} />
+    </ArrowIcon>
+  </ArrowContainer>
+
+}
--- src/containers/homepage/projectsSection.jsx
+++ src/containers/homepage/projectsSection.jsx
@@ -1,26 +1,49 @@
 import React from 'react'
-import {Element} from 'react-scroll'
+import { Element, scroller } from 'react-scroll'
 import styled from 'styled-components'
 
 import {SectionTitle} from '../../components/sectionTitle/index'
+import { DownArrow } from "../../components/downArrow/index"
+import { UpArrow } from "../../components/upArrow/index"
+import { Marginer } from "../../components/marginer";
+
 
 const ProjectContainer = styled(Element)`
   width: 100%;
   height: 100vh;
-  min-height: 400px;
   display: flex;
   flex-direction: column;
-
-
 `;
 
 
+const UpArrowContainer = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`
+
 
 export function ProjectSection(props) {
-    return (
-    <ProjectContainer name ="projectSection">
-      <SectionTitle>Here are some of my projects</SectionTitle>
+  const scrollToNextSection = () => {
+    scroller.scrollTo("aboutMe", { smooth: true, duration: 1500})
+  }
 
+    return (
+      <Element name="projectSection">
+    <ProjectContainer> 
+    <Marginer direction="vertical" margin="4em" />
+
+
+       
+      
+      <SectionTitle>Here are some of my projects</SectionTitle>
+      <UpArrowContainer onClick={scrollToNextSection} >
+      <UpArrow/>
+        </UpArrowContainer>
+        
+        
+        
     </ProjectContainer>
+    </Element>
     )
 }
